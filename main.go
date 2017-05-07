8d538269ae6a95ee76e1c2296980c4954d038aa7
Rene Martin <rene_martin@intuit.com>
2017-05-07 19:05:05 +0100

    Static analysis verification in travis

--- main.go
+++ main.go
@@ -19,27 +19,11 @@ var (
 )
 
 // Init Initializes the logging system
-func Init(
-  traceHandle io.Writer,
-  infoHandle io.Writer,
-  warningHandle io.Writer,
-  errorHandle io.Writer) {
-
-  Trace = log.New(traceHandle,
-    "TRACE: ",
-    log.Ldate|log.Ltime|log.Lshortfile)
-
-  Info = log.New(infoHandle,
-    "INFO: ",
-    log.Ldate|log.Ltime|log.Lshortfile)
-
-  Warning = log.New(warningHandle,
-    "WARNING: ",
-    log.Ldate|log.Ltime|log.Lshortfile)
-
-  Error = log.New(errorHandle,
-    "ERROR: ",
-    log.Ldate|log.Ltime|log.Lshortfile)
+func Init(traceHandle io.Writer, infoHandle io.Writer, warningHandle io.Writer, errorHandle io.Writer) {
+  Trace = log.New(traceHandle, "TRACE: ", log.Ldate|log.Ltime|log.Lshortfile)
+  Info = log.New(infoHandle, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+  Warning = log.New(warningHandle, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
+  Error = log.New(errorHandle, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -48,7 +32,6 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func getenv(key, fallback string) string {
-
   value := os.Getenv(key)
   if len(value) == 0 {
     return fallback
